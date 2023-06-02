70054074051e36d3c5ff081c474fd2203c133250
James Meegan <58824024+jmeegan2@users.noreply.github.com>
2023-06-01 22:01:28 -0400

    Initial Commit

---
+++ Flashcard Place/Flashcard_PlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashcard_PlaceApp.swift
+//  Flashcard Place
+//
+//  Created by James Meegan on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Flashcard_PlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
